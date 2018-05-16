fb6d3e2d3cf5ca128b6c1c9330c732e6406f3e03
andrew-leith <andrew-leith@users.noreply.github.com>
2018-05-16 15:31:05 -0400

    incremental update, do not run

--- newparser.py
+++ newparser.py
@@ -3,6 +3,9 @@
 
 # Create a function just for a single reference, have that be the 'single' behavior, and then loop over it for the 'multi'
 # maybe have two separate argparse flags that determine behavior from the outset
+# Create one function, multiindex() or something, to generate indices for all tools provided in a separate subheading of the master yaml
+# 	can a yaml have multiple values for one key??? - https://learnxinyminutes.com/docs/yaml/ ctrl+f 'json'
+
 
 
 import argparse
@@ -18,11 +21,28 @@ import collections
 ######################################
 
 
+def append(origin, destination):
+	"""Append the 'new' YAML to the 'master' YAML"""
+
+	print("Now appending --new to --master...")
+	subprocessCommand = 'grep -n "reference-entries" ' + origin + ' | grep -Eo \'^[^:]+\''
+	referenceLine = int(subprocess.check_output([subprocessCommand],shell=True))
+	subprocessCommand = 'wc -l < ' + origin
+	totalLines = int(subprocess.check_output([subprocessCommand],shell=True))
+	tailLines = totalLines - referenceLine
+	subprocessCommand = 'tail -n' + str(tailLines) + ' ' + origin + '> temp.yaml'
+	subprocess.call([subprocessCommand], shell=True)
+
+	subprocessCommand = "cat " + origin + " >> " + destination
+	subprocess.call([subprocessCommand], shell=True)
+	#subprocess.call(['rm temp.yaml'], shell=True)
+	print("Done")
 
 class referenceHandler:
 	def __init__(self, filetype="yaml", errorBehavior="False"):
 		self.filetype = filetype
 		self.errorBehavior = errorBehavior
+
 
 	def retrieveReference(self, rootSubDirectory, yamlEntry, componentName):
 		"""Create a folder named for the reference component, then download and process the reference files in that folder.
@@ -126,9 +146,52 @@ class referenceHandler:
 			# these can be named anything and there can be any number of them
 
 
+# Create argument parser
+parser = argparse.ArgumentParser(description='Controls how to run the reference parser')
+parser.add_argument('-e', '--execute', help = 'Executes the YAML file, either the new if it exists or the master if not', action='store_true')
+parser.add_argument('--master', type=str, required = True, help = 'Denotes the Master YAML')
+parser.add_argument('--new', type=str, help = 'Denotes the new YAML')
+parser.add_argument('--skip', help = 'Skip appending the new YAML (mainly for testing)', action="store_true")
+# check for --master and --new FIRST to determine the mode, then check --execute to determine what to do
+# do not allow running both the --new and the --master in one command
+# if --new exists and --execute is TRUE, run the --new and append --new to --master (subject to --skip in testing)
+# if --new exists and --execute is FALSE, do not run anything and just append --new to --master (subject to --skip in testing)
+# if --new does not exist and --execute is TRUE, run the --master
+# if --new does not exist and --execute is FALSE, exit with an error, as this combination is pointless
+
+
+
+
+
+# Parse arguments
+arguments = parser.parse_args()
+
+if arguments.new is None:
+	if arguments.execute:
+		print("No new YAML detected - running the master only...")
+		# load the master as yamlPar
+		yamlPar = yaml.load(open(arguments.master))
+	else:
+		sys.exit("Nothing to do - exiting...")
+		# load nothing, do nothing
+else:
+# if --new exists
+	if arguments.execute:
+		print("Running new and appending to master...")
+		# load the new as yamlPar and append it after running; no need to load master
+		yamlPar = yaml.load(open(arguments.new))
+	else:
+		print("Appending to master with no execution...")
+		# load nothing, just append new to master using >>
+		append(arguments.new, arguments.master)
+
+
+# Begin parsing of YAML
+#yamlPar = yaml.load(open(arguments.yaml))
+
 
 if  __name__ == "__main__":
-	yamlPar = yaml.load(open("/users/aleith/reference_yaml/prototype.yaml"))
+	#yamlPar = yaml.load(open("/users/aleith/reference_yaml/prototype.yaml"))
 	rootDirectory = yamlPar["reference-yaml"]["configuration"]["root-directory"]
 	referenceKeys = sorted(yamlPar["reference-yaml"]["reference-entries"].keys(), key=lambda entry: int(entry.split('-')[2]))
 	# extract the keys under 'reference-entries' named 'reference-information-X'
@@ -136,6 +199,8 @@ if  __name__ == "__main__":
 	for k in range(0, len(referenceKeys)):
 		run.processEntry(rootDirectory, yamlPar["reference-yaml"]["reference-entries"].get(referenceKeys[k]))
 		# run processEntry for each 'reference-information-X'
+
+
 
 
 

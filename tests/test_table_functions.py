5a223570cfa6001e44a9395ec6f9e1121327798b
fernandogelin <fernandogelin@gmail.com>
2018-10-29 16:55:25 -0400

    adds another test and comments

--- tests/test_table_functions.py
+++ tests/test_table_functions.py
@@ -1,12 +1,12 @@
 import pytest
 from refchef.table_functions import *
 
-@pytest.fixture
+@pytest.fixture # macro to set up a fixture that will be used in other functions.
 def menu():
     menu = get_full_menu("tests/data/example.yml")
     return menu
 
-def test_table_columns(menu):
+def test_table_columns(menu): #takes the fixture created above as an argument.
     assert menu.shape == (6,6)
     assert list(menu) == ['downloader', 'name', 'organization', 'species', 'type', 'component']
 
@@ -15,3 +15,7 @@ def test_filter(menu):
     assert filtered.shape == (5,6)
     for i in list(filtered["species"]):
         assert i == "human"
+
+    filtered2 = filter_menu(menu, "type", "references")
+    for i in list(filtered2["type"]):
+        assert i == "references"

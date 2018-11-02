127e300e83586ffde4472b89f22fdbf16b05011c
andrew-leith <andrew-leith@users.noreply.github.com>
2018-11-01 22:24:01 -0400

    first attempt at reference handler test

---
+++ tests/test_references.py
@@ -0,0 +1,21 @@
+import pytest
+from refchef.references import *
+from refchef.utility_functions import *
+import argparse
+import os
+import subprocess
+import yaml
+import time
+import sys
+import datetime
+import collections
+from collections import OrderedDict, defaultdict
+
+def test_references():
+    data = ordered_load(open("tests/data/test_master.yaml"))
+    rootDirectory = "tests/data/"
+    referenceKeys = data.keys()
+    run = referenceHandler(errorBehavior = False)
+    for k in range(0, len(referenceKeys)):
+		run.processEntry(rootDirectory, data.get(referenceKeys[k]))
+	assert os.path.isfile("tests/data/ucsc_mm9_chr1/primary/final_checksums.md5") 

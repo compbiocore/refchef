f28cbddb08d531344d45ffa3364307ad34b683ef
andrew-leith <andrew-leith@users.noreply.github.com>
2018-11-04 22:46:13 -0500

    test config

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,9 +12,10 @@ from collections import OrderedDict, defaultdict
 import yamlloader
 
 
-def test_generate_config_3():
+def test_generate_config_3(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda x: "dummy")
     test_conf = config_file()
-    test_conf.input = lambda: 'dummy'
+    #test_conf.input = lambda: 'dummy'
     test_conf.generate_config_3()
     os.path.exists("tests/data/config.yaml")
     # ordered_load reads in a YAML as an ordered dictionary, so its type should be OrderedDict

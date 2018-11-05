4c19d41d0caaf01fe09c211992375d63607fdd8b
andrew-leith <andrew-leith@users.noreply.github.com>
2018-11-04 23:03:12 -0500

    adjusted test

--- tests/test_config.py
+++ tests/test_config.py
@@ -15,8 +15,6 @@ import yamlloader
 def test_generate_config_3(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda x: "yes")
     test_conf = config_file()
-    #test_conf.input = lambda: 'dummy'
     test_conf.generate_config_3()
     os.listdir(".")
-    assert os.path.exists("tests/config.yaml")
-    # ordered_load reads in a YAML as an ordered dictionary, so its type should be OrderedDict
+    assert os.path.exists("config.yaml")

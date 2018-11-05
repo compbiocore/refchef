c814d1f13e227a6a6cbb339b7740eafd6062c5dd
andrew-leith <andrew-leith@users.noreply.github.com>
2018-11-04 23:15:24 -0500

    removed unneeded line

--- tests/test_config.py
+++ tests/test_config.py
@@ -20,5 +20,4 @@ def test_generate_config_3(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda x: "yes")
     test_conf = config_file()
     test_conf.generate_config_3()
-    os.listdir(".")
     assert os.path.exists("config.yaml")

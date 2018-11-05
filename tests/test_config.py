d87d71986d8d9b2590751e575a9e469b29264d15
andrew-leith <andrew-leith@users.noreply.github.com>
2018-11-04 22:50:50 -0500

    adjusted test

--- tests/test_config.py
+++ tests/test_config.py
@@ -13,7 +13,7 @@ import yamlloader
 
 
 def test_generate_config_3(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda x: "dummy")
+    monkeypatch.setattr('builtins.input', lambda x: "yes")
     test_conf = config_file()
     #test_conf.input = lambda: 'dummy'
     test_conf.generate_config_3()

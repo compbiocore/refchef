5d20b88ca2ee65fce1240e6ada370376e2dec1b7
andrew-leith <andrew-leith@users.noreply.github.com>
2018-11-06 22:25:32 -0500

    updated references.py

--- refchef/references.py
+++ refchef/references.py
@@ -121,9 +121,11 @@ class referenceHandler:
 
 		for i in allReferences:
 			self.retrieveReference(referenceParentLocation, i, i["component"])
-		for j in all_annotations:
-			self.retrieveReference(referenceParentLocation, j, j["component"])
-		for k in all_indices:
-			self.retrieveReference(referenceParentLocation, k, k["component"])
-			# retrieve each component of the reference e.g. 'primary reference', 'est', 'gtf', etc
+		if len(all_annotations) > 0:
+			for j in all_annotations:
+				self.retrieveReference(referenceParentLocation, j, j["component"])
+		if len(all_indices) > 0:
+			for k in all_indices:
+				self.retrieveReference(referenceParentLocation, k, k["component"])
+				# retrieve each component of the reference e.g. 'primary reference', 'est', 'gtf', etc
 			# these can be named anything and there can be any number of them

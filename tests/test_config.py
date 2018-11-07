951ac7c1d622806fc726fdc96220571263161c00
fernandogelin <fernandogelin@gmail.com>
2018-11-07 12:31:44 -0500

    travis issue

--- tests/test_config.py
+++ tests/test_config.py
@@ -18,7 +18,7 @@ def test_generate_config(monkeypatch):
     test_conf = config_file()
     test_conf.generate_config("tests/data/.refchef.config")
     assert os.path.exists(os.path.expanduser("tests/data/.refchef.config"))
-    shutil.copy("tests/data/.refchef.config", ".refchef-config")
+    shutil.copy("tests/data/.refchef.config", os.path.expanduser("~/.refchef-config"))
 
 def test_config():
     c = Config("tests/data")

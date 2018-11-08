c71c82c61311e88ded56e5c47b90211feed3ef19
fernandogelin <fernandogelin@gmail.com>
2018-11-08 14:40:38 -0500

    updates tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -46,7 +46,10 @@ def test_logical():
 
 def test_add_path():
     s = "md5 *.fa > final_checksums.md5"
-    assert add_path(s, "test/") == "md5 test/*.fa > test/final_checksums.md5"
+    if sys.platform == 'darwin':
+        assert add_path(s, "test/") == "md5 test/*.fa > test/final_checksums.md5"
+    else:
+        assert add_path(s, "test/") == "md5sum test/*.fa > test/final_checksums.md5"
 
     s2 = "gunzip *.gz"
     assert add_path(s2, "test/") == "gunzip test/*.gz"

e5c6f247c990d1f314f0782f9a3b4c1ce6c3619d
fernandogelin <fernandogelin@gmail.com>
2018-11-19 08:30:34 -0500

    minor updates to docs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="refchef",
-    version="0.0.1",
+    version="0.0.2",
     author="Andrew Leith & Fernando Gelin",
     author_email="aleith@brown.edu",
     description="Genome reference manager.",

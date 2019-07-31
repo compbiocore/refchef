205c31ea164f5a1c357b681c6fb0bddf69b804e4
semantic-release <semantic-release>
2019-07-31 21:44:39 +0000

    1.0.0
    Automatically generated by python-semantic-release

--- refchef/__init__.py
+++ refchef/__init__.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import oyaml as yaml
 
-__version__ = '0.1.4'
+__version__ = '1.0.0'
 
 # load dotenv in the base root
 APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
